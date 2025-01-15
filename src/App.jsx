3f15896c492a1fabf6e1521b5cb43abc612eee63
MatiasPerez00 <matiasperez.personal@gmail.com>
2025-01-15 09:42:25 -0300

    feat:añade las rutas para su utilizacion en el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-// import { Route, Routes } from 'react-router-dom'
+import { Navigate,Route, Routes } from 'react-router-dom'
 import './App.css'
 import NavComponent from './assets/Components/Layout/Header/Navbar/NavComponent.jsx'
 import FooterComponent from './assets/Components/Layout/Footer/FooterComponent.jsx';
-
+// import PrivateRoute from './PrivateRoute.jsx';
 
 function App() {
 
   return (
     <div className="AppCAL">
       <NavComponent/>
-      {/* <Routes>
-        <Route>
-        
-        </Route>
-      </Routes> */}
-      <img src="https://drive.google.com/uc?export=view&id=1vF3g-S1YX6WxPZ7jusQbOD_SP8LFjBIu" alt="" width={300} height={300} />
+       <Routes>
+        <Route path="/" element={<Navigate to={"/Home"} />} />
+        <Route path='/Home' element={<HomePage /*user={user}*/ />} />
+        <Route path="/Register" element={<RegisterForm />} />
+        <Route path="/AboutTeam" element={<AboutTeam />} />
+        <Route path="/Login" element={<LoginPage setUser={setUser} />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Galery" element={<Gallery />} />
+        {/* <Route path='/Reserve' 
+               element={
+                <PrivateRoute isAllowed={user.isLoggedIn}>
+                <Reservas />
+                </PrivateRoute>
+                      } 
+        />
+        <Route path='/Admin' 
+          element={
+                  <PrivateRoute isAllowed={user.isLoggedIn && user.role === "Administrador"}>
+                  <Admin />
+                  </PrivateRoute>
+                  } 
+        /> */}
+        {/* <Route path="*" element={<Error404 />} /> */}
+      </Routes>
+      
       <FooterComponent/>
     </div>
   )
